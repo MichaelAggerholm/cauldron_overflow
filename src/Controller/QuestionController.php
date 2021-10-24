7c6131a3af7a76b63b417c0aa11b5e110a14ec62
Michael Aggerholm <92aggerholm@gmail.com>
2021-10-24 23:17:40 +0200

    practicing twig build in functions and calls.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
 	/**
 	 * @Route("/")
@@ -20,9 +21,15 @@ class QuestionController
 	 */
 	public function show($slug)
 	{
-		return new Response(sprintf(
-			'Future page to show the question "%s"!',
-			ucwords(str_replace('-', ' ', $slug))
-		));
+		$answers = [
+			'This is my fake answer numbre 1',
+			'This is my fake answer numbre 2',
+			'This is my fake answer numbre 3',
+		];
+
+		return $this->render('question/show.html.twig', [
+			'question' => ucwords(str_replace('-', ' ', $slug)),
+			'answers' => $answers,
+		]);
 	}
 }
