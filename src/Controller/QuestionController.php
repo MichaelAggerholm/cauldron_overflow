18e8ee30a0bbb508af65f85ae8885fbb945e4931
Michael Aggerholm <92aggerholm@gmail.com>
2021-10-27 19:26:07 +0200

    practice with debugging.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -27,6 +27,8 @@ class QuestionController extends AbstractController
 			'This is my fake answer numbre 3',
 		];
 
+//		dump($slug, $this);
+
 		return $this->render('question/show.html.twig', [
 			'question' => ucwords(str_replace('-', ' ', $slug)),
 			'answers' => $answers,
