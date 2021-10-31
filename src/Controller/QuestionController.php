97745bb61e032c428380d6003f2ab67b9fb72a71
Michael Aggerholm <92aggerholm@gmail.com>
2021-10-31 17:25:37 +0100

    updating files with course files since author included files for practice.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,65 +27,31 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(QuestionRepository $repository)
     {
-        return $this->render('question/homepage.html.twig');
+        $questions = $repository->findAllAskedOrderedByNewest();
+
+        return $this->render('question/homepage.html.twig', [
+            'questions' => $questions,
+        ]);
     }
 
-	/**
-	 * @Route("/questions/new")
-	 */
-	public function new(EntityManagerInterface $entityManager)
-	{
-		$question = new Question();
-		$question->setName('Missing pants')
-			->setSlug('missing-pants-'.rand(0, 1000))
-			->setQuestion(<<<EOF
-				Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-				to make my dishes wash themselves. But while I was casting it,
-				I slipped a little and I think `I also hit my pants with the spell`.
-				When I woke up this morning, I caught a quick glimpse of my pants
-				opening the front door and walking out! I've been out all afternoon
-				(with no pants mind you) searching for them.
-				Does anyone have a spell to call your pants back?
-			EOF
-			);
-
-		if (rand(1, 10) > 2)
-		{
-			$question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-		}
-
-		// Saving to database by using doctrine. this can be done to multiple objects at once, in this case.. just once.
-		$entityManager->persist($question); // Hey doctrine, please be aware of this question object.
-		$entityManager->flush(); // Yo doctrine, please make a query and save all the objects that you are aware of.
-
-		return new Response(sprintf(
-			// placeholders
-			'Well hallo! The shiny new question is id #%d, slug %s',
-			// placeholder values
-			$question->getId(),
-			$question->getSlug(),
-		));
-	}
+    /**
+     * @Route("/questions/new")
+     */
+    public function new()
+    {
+        return new Response('Sounds like a GREAT feature for V2!');
+    }
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
-
-		$repository = $entityManager->getRepository(Question::class);
-		/** @var Question|null $question */
-		$question = $repository->findOneBy(['slug' => $slug]);
-		if (!$question)
-		{
-			throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
-		}
-//		dd($question);
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -94,6 +62,26 @@ class QuestionController extends AbstractController
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers,
+        ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up') {
+            $question->upVote();
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug()
         ]);
     }
 }
