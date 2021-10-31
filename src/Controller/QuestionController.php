419be29d704eabfc80652b982871bc4a8191decf
Michael Aggerholm <92aggerholm@gmail.com>
2021-10-31 14:59:16 +0100

    testing first save to db with doctrine using persist and flush.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +29,43 @@ class QuestionController extends AbstractController
     {
         return $this->render('question/homepage.html.twig');
     }
+
+	/**
+	 * @Route("/questions/new")
+	 */
+	public function new(EntityManagerInterface $entityManager)
+	{
+		$question = new Question();
+		$question->setName('Missing pants')
+			->setSlug('missing-pants-'.rand(0, 1000))
+			->setQuestion(<<<EOF
+				Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+				to make my dishes wash themselves. But while I was casting it,
+				I slipped a little and I think `I also hit my pants with the spell`.
+				When I woke up this morning, I caught a quick glimpse of my pants
+				opening the front door and walking out! I've been out all afternoon
+				(with no pants mind you) searching for them.
+				Does anyone have a spell to call your pants back?
+			EOF
+			);
+
+		if (rand(1, 10) > 2)
+		{
+			$question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+		}
+
+		// Saving to database by using doctrine. this can be done to multiple objects at once, in this case.. just once.
+		$entityManager->persist($question); // Hey doctrine, please be aware of this question object.
+		$entityManager->flush(); // Yo doctrine, please make a query and save all the objects that you are aware of.
+
+		return new Response(sprintf(
+			// placeholders
+			'Well hallo! The shiny new question is id #%d, slug %s',
+			// placeholder values
+			$question->getId(),
+			$question->getSlug(),
+		));
+	}
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
